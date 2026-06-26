7d8d3c07c291b895a7f2607b61936f95e63e6516
LonelyGuy-SE1 <brawlers.assemble@gmail.com>
2026-06-26 13:15:02 +0530

    docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,21 @@
-#run using uv run fastapi dev 
+from pathlib import Path
 
 from fastapi import FastAPI
-from app.api import router
-#from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
+from app.api import router
 
-app=FastAPI(title="PESU Curriculum Automation")
-"""
+app = FastAPI(title="PESU Curriculum Automation")
+
 app.add_middleware(
     CORSMiddleware,
-     allow_origins=["*"],
-     allow_methods=["*"],
-     allow_headers=["*"],
-)"""
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.include_router(router, prefix="/api")       
+app.include_router(router, prefix="/api")
 
-app.mount("/", StaticFiles(directory="../frontend", html=True), name="frontend")
+if Path("../frontend").exists():
+    app.mount("/", StaticFiles(directory="../frontend", html=True), name="frontend")
